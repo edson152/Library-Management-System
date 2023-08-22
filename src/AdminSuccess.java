b4efd947885867e2f6c3e77de1bdd474d72b84a7
edson152 <71998101+edson152@users.noreply.github.com>
2023-08-23 05:45:12 +0800

    first comit

---
+++ src/AdminSuccess.java
@@ -0,0 +1,122 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+
+public class AdminSuccess extends JFrame {
+	static AdminSuccess frame;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new AdminSuccess();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminSuccess() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 371);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblAdminSection = new JLabel("Admin Section");
+		lblAdminSection.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblAdminSection.setForeground(Color.GRAY);
+		
+		JButton btnNewButton = new JButton("Insert Librarian");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			LibrarianForm.main(new String[]{});
+			frame.dispose();
+			}
+		});
+
+		
+		
+		JButton btnViewLibrarian = new JButton("View Librarian");
+		btnViewLibrarian.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			ViewLibrarian.main(new String[]{});
+			}
+		});
+		btnViewLibrarian.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JButton btnDeleteLibrarian = new JButton("Delete Librarian");
+		btnDeleteLibrarian.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			DeleteLibrarian.main(new String[]{});
+			frame.dispose();
+			}
+		});
+		btnDeleteLibrarian.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Library.main(new String[]{});
+				frame.dispose();
+			}
+		});
+		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(152, Short.MAX_VALUE)
+					.addComponent(lblAdminSection, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
+					.addGap(123))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(134)
+					.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(111, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(24)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(btnDeleteLibrarian, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
+						.addComponent(btnViewLibrarian, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))
+					.addContainerGap(268, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblAdminSection, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnViewLibrarian, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnDeleteLibrarian, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(23, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
